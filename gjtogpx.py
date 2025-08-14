ff6c8829080c21f59dc77250b1e5a0b505cc9bad
Brian "Beej Jorgensen" Hall <beej@beej.us>
2025-08-13 17:29:47 -0700

    add osmand functionality

--- gjtogpx.py
+++ gjtogpx.py
@@ -13,11 +13,13 @@ import re
 from xml.sax.saxutils import escape
 
 class Waypoint:
-    def __init__(self, name, lat, lon, sym, desc=None):
+    def __init__(self, name, lat, lon, garmin_sym, osmand_sym, color, desc=None):
         self.name = name
         self.lat = lat
         self.lon = lon
-        self.sym = sym
+        self.garmin_sym = garmin_sym
+        self.osmand_sym = osmand_sym
+        self.color = color
         self.desc = desc
 
 class Track:
@@ -38,8 +40,10 @@ def toxml(name, waypoints, tracks):
     """
 
     r = '<?xml version="1.0"?><gpx version="1.0" creator="gjwaypoints" ' \
-        'xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" ' \
         'xmlns="http://www.topografix.com/GPX/1/0" ' \
+        'xmlns:osmand="https://osmand.net/docs/technical/osmand-file-formats/osmand-gpx" ' \
+        'xmlns:gpxtpx="https://www8.garmin.com/xmlschemas/TrackPointExtensionv1.xsd" ' \
+        'xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" ' \
         'xsi:schemaLocation="http://www.topografix.com/GPX/1/0 ' \
         'http://www.topografix.com/GPX/1/0/gpx.xsd">' \
         f'<name>{name}</name>'
@@ -47,9 +51,14 @@ def toxml(name, waypoints, tracks):
     for w in waypoints:
         r += f'<wpt lat="{w.lat}" lon="{w.lon}">'
         r += f'<name>{escape(w.name)}</name>'
-        r += f'<sym>{w.sym}</sym>'
+        r += f'<sym>{w.garmin_sym}</sym>'
         if w.desc is not None:
             r += f'<cmt>{escape(w.desc)}</cmt>'
+        r += '<extensions>'
+        r += f'<osmand:color>{w.color}</osmand:color>'
+        r += f'<osmand:icon>{w.osmand_sym}</osmand:icon>'
+        r += f'<osmand:background>circle</osmand:background>'
+        r += '</extensions>'
         r += '</wpt>'
 
     for t in tracks:
@@ -70,13 +79,7 @@ def toxml(name, waypoints, tracks):
 
     return r
 
-def symbol_map(sym, name, color):
-    # All red markers should be red-flagged
-    if color == "FF0000" or color == "#FF0000":
-        return "Flag, Red"
-
-    sym = sym.split('$')[0]
-
+def sym_normalize(sym, name):
     placemark2_map = {
         "restroom|bathroom|washroom|toilet": "restroom",
         "ranger sta|guard sta": "residence",
@@ -102,18 +105,12 @@ def symbol_map(sym, name, color):
     }
 
     if sym == "placemark2":
-        found = False
+        sym = None
 
         for key in placemark2_map:
             if re.search(key, name, re.I) is not None:
                 sym = placemark2_map[key]
-                found = True
                 break
-
-        if not found:
-            print(f'Unknown name {name} for placemark2',
-                    file=sys.stderr)
-            sym = "pin"
 
     if sym == "danger" and re.search(r"cemetery|grave", name, re.I) is not None:
         sym = "cemetery"
@@ -127,6 +124,20 @@ def symbol_map(sym, name, color):
 
     if sym in ("camping", "campfire") and re.search(r"campsite", name, re.I) is not None:
         sym = "campsite"
+
+    return sym
+
+def garmin_symbol_map(sym, name, color):
+    # All red markers should be red-flagged
+    if color == "FF0000" or color == "#FF0000":
+        return "Flag, Red"
+
+    sym = sym.split('$')[0]
+    sym = sym_normalize(sym, name)
+
+    if sym is None:
+        print(f'Unknown name {name} for placemark2', file=sys.stderr)
+        sym = "pin"
 
     sym_map = {
         "airport": "Airport",
@@ -199,6 +210,84 @@ def symbol_map(sym, name, color):
 
     return sym_map[sym]
 
+def osmand_symbol_map(sym, name, color):
+    sym = sym.split('$')[0]
+    sym = sym_normalize(sym, name)
+
+    new_color = "#0000ff"  # blue
+
+    if sym is None:
+        #print(f'Unknown name {name} for placemark2', file=sys.stderr)
+        sym = "special_flag"
+
+    sym_map = {
+        "airport": "air_transport",
+        "atv": "special_utv",
+        "automobile": "shop_car",
+        "bar": "amenity_pub",
+        "bicycling-downhill": "special_bicycle",
+        "bridge": "bridge_structure_arch",
+        "camping": "tourism_camp_site",
+        "campsite": "firepit",
+        "carrepair": "shop_car_repair",
+        "caving": "natural_cave_entrance",
+        "cemetery": "cemetery",
+        "circle-p": "amenity_parking",
+        "crossing": "level_crossing",
+        "danger": "hazard",
+        "drinking-water": "amenity_drinking_water",
+        "firelookout": "observation_tower",
+        "flag-1": "special_flag",   # Black flag
+        "flag-2": "special_flag_finish",  # Checkered Flag
+        "foodservice": "restaurants",
+        "fuel": "amenity_fuel",
+        "gate-side": "barrier_gate",
+        "hiking": "special_trekking",
+        "hunting": "hunting",
+        "hut": "special_house",
+        "info": "special_information",
+        "junction": "junction",
+        "lake": "water",
+        "laundry": "shop_laundry",
+        "lodging": "tourism_hotel",
+        "mine": "man_made_mineshaft",
+        "movingvan": "special_truck",
+        "museum": "tourism_museum",
+        "peak": "natural",   # natural_peak
+        "photo": "photo",
+        "picnicbench": "tourism_picnic_site",
+        "pin": "special_flag",
+        "postoffice": "amenity_post_office",
+        "radiotower": "communication_tower",
+        "rangerstation2": "ranger_station",
+        "residence": "special_house",
+        "restarea": "rest_area",
+        "restroom": "amenity_toilets",
+        "shelter-empty": "amenity_shelter",
+        "shopping": "shop_supermarket",
+        "summit": "natural",
+        "swimming": "swimming_pool",
+        "theater": "amenity_cinema",
+        "usar-20": "access_no",    # Do Not Enter
+        "warning": "special_flag",
+        "waterfalls": "waterfall",
+        "wilderness": "forest",
+    }
+
+    # https://osmand.net/docs/technical/osmand-file-formats/osmand-gpx/
+    # https://github.com/mariush444/gmapIcons2osmand/blob/main/icons-gmap-osmand.pdf
+
+    # Force red if asked
+    if color == "FF0000" or color == "#FF0000" or sym == "usar-20" or sym == "warning":
+        new_color = "#ff0000"
+
+    if sym in sym_map:
+        sym = sym_map[sym]
+    else:
+        #print(f'Symbol "{sym}" not in symbol map', file=sys.stderr)
+        sym = "special_flag"
+
+    return sym, new_color
 
 def get_waypoints_tracks(jdata):
     waypoints = []
@@ -225,7 +314,8 @@ def get_waypoints_tracks(jdata):
             name = f["properties"]["title"]
             color = f["properties"]["marker-color"] if "marker-color" in f["properties"] else "#000000"
 
-            sym = symbol_map(sym, name, color)
+            garmin_sym = garmin_symbol_map(sym, name, color)
+            osmand_sym, color = osmand_symbol_map(sym, name, color)
 
             if "description" in f["properties"]:
                 desc = f["properties"]["description"]
@@ -237,7 +327,7 @@ def get_waypoints_tracks(jdata):
             c = f["geometry"]["coordinates"]
             c = list(map(lambda x: float(f'{x:.6f}'), c))
 
-            wp = Waypoint(name, c[1], c[0], sym, desc); # switch to lat,lon
+            wp = Waypoint(name, c[1], c[0], garmin_sym, osmand_sym, color, desc); # switch to lat,lon
 
             waypoints.append(wp)
 
